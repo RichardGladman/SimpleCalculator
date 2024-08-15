68ff59774d9ca845f21007cd4d2f4c5db93d67fe
Richard Gladman <richard@thefifthcontinent.com>
2024-08-15 16:24:48 +0100

    Parse and validate input paramters.
    Scaffold main functions
    ======
    file7.txt. Changed in br-3.
    >>>>>>> br-3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 
-using namespace std;
+void interactive_mode();
+void calculate(double operand1, double operand2, char op);
 
-int main()
+int main(int argc, char **argv)
 {
+    if (argc == 1) { // No arguments passed, go interactive
+        interactive_mode();
+        return 0;
+    }
+
+    if (argc != 4) { // Batch mode. Three arguments must be passed
+        std::cerr << "SimpleCalculator is running in batch mode. You must pass exactly three paramters. For example:\n";
+        std::cerr << "\tSimpleCalculator 2 + 2" << std::endl;
+        return 1;
+    }
+
+    double operand1 = std::atof(argv[1]);
+    double operand2 = std::atof(argv[3]);
+
+    char op = argv[2][0];
+
+    if (op != '+' && op != '-' && op != 'x' && op != '/' && op != '%') {
+        std::cerr << "SimpleCalculator understands the operators +, -, x, /, &" << std::endl;
+        return 1;
+    }
+
+    calculate(operand1, operand2, op);
+
     return 0;
+}
+
+void interactive_mode()
+{
+
+}
+
+void calculate(double operand1, double operand2, char op)
+{
+
 }
